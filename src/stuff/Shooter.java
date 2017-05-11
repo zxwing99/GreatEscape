e06fae066fe47fa1e11c9ee7b895a2cb95b75fe5
zacha <zacha@10.16.95.52>
2017-05-11 13:29:12 -0700

    this one

--- src/stuff/Shooter.java
+++ src/stuff/Shooter.java
@@ -1,10 +1,15 @@
 package stuff;
 
 public class Shooter extends Obstacle {
+	int direction;
 
 	public Shooter(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setDirection(int direction){
+		this.direction = direction;
 	}
 
 	
