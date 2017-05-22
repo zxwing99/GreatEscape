dd698a28d359165a7760fbf934c4309d0c59bb7e
Zachary <Zachary@192.168.0.63>
2017-05-21 17:06:30 -0700

    poof

--- src/maps/Level2.java
+++ src/maps/Level2.java
@@ -58,11 +58,41 @@ public class Level2 extends LevelMap{
 		obstacles.add(new Block(450, -650, 100, 5));
 		obstacles.add(new Block(550, -650, 5, 200));
 		
+		obstacles.add(new Block(150, -1100, 5, 250));
+		obstacles.add(new Block(150, -1100, 100, 5));
+		obstacles.add(new Block(250, -1100, 5, 250));
+		
+		obstacles.add(new Block(350, -1100, 5, 250));
+		obstacles.add(new Block(350, -1100, 100, 5));
+		obstacles.add(new Block(450, -1100, 5, 250));
+		
+		obstacles.add(new Block(550, -1100, 5, 250));
+		obstacles.add(new Block(550, -1100, 100, 5));
+		obstacles.add(new Block(650, -1100, 5, 250));
+		
+		obstacles.add(new Block(0, -1200, 250, 5));
+		obstacles.add(new Block(0, -1300, 150, 5));
+		
+		obstacles.add(new Block(400, -1250, 350, 5));
+		obstacles.add(new Block(400, -1350, 350, 5));
+		
+		obstacles.add(new Block(100, -1400, 100, 5));
+		obstacles.add(new Block(200, -1500, 5, 100));
+		
+		obstacles.add(new Block(300, -1500, 5, 100));
+		obstacles.add(new Block(300, -1400, 100, 5));
+		
+		obstacles.add(new Block(500, -1500, 250, 5));
+		
 		obstacles.add(new Shooter(100, 450, 50, 50, 0));
 		obstacles.add(new Shooter(450, 250, 50, 50, 90));
 		obstacles.add(new Shooter(650, -200, 50, 50, 315));
 		obstacles.add(new Shooter(300, -400, 50, 50, 90));
 		obstacles.add(new Shooter(200, -700, 50, 50, 0));
+		obstacles.add(new Shooter(500, -800, 50, 50, 90));
+		obstacles.add(new Shooter(50, -1150, 50, 50, 315));
+		obstacles.add(new Shooter(200, -1350, 50, 50, 90));
+		obstacles.add(new Shooter(550, -1200, 50, 50, 0));
 		
 	}
 
